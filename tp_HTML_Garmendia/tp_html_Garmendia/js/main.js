d8083c37e68d54035b73a920fb0fa2d0a694e102
Valentino Garmendia <128641090+vgarmendia@users.noreply.github.com>
2024-12-21 19:18:15 -0300

    Add files via upload

---
+++ tp_HTML_Garmendia/tp_html_Garmendia/js/main.js
@@ -0,0 +1,5 @@
+// Abrir o cerrar menú lateral
+document.getElementById("columnatoggle").addEventListener("click", function () {
+    const columna = document.getElementById("menuColumna");
+    columna.classList.toggle("ocultar");
+});
